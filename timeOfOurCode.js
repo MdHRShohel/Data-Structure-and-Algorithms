5473d2d5f813a945323d91b1c3262eb77dd9eaf0
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2023-05-30 02:50:01 +0600

    time complexity

--- timeOfOurCode.js
+++ timeOfOurCode.js
@@ -11,6 +11,23 @@ function sumOfAll2(n){
 }
 
 let time1 = performance.now();
-console.log(sumOfAll2(6600000000));
+//console.log(sumOfAll2(6600000000));
 let time2 = performance.now();
-console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
+//console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
+
+function minLog5(n){
+    for(let i = 1; i <= Math.min(5, n); i++){
+        console.log(i);
+    }
+}
+
+//minLog5(16); //time complexity is O(1) because it will only run 5 times
+
+function maxLog5(n){
+    for(let i = 1; i <= Math.max(5, n); i++){
+        console.log(i);
+    }
+}
+
+maxLog5(1);//time complexity is O(n) because it will run n times
+
